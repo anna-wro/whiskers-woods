f68acc61832b983c3a62f656c1389e8f1b38ef99
Anna Wróbel <wro.online@gmail.com>
2023-07-18 17:27:27 +0200

    🎨 Background adjustments and proportions

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -1,5 +1,5 @@
 export const CANVAS_WIDTH = 800;
-export const CANVAS_HEIGHT = 600;
+export const CANVAS_HEIGHT = 500;
 export const CELL_SIZE = 20;
 export const VISITED_RADIUS = CELL_SIZE / 2;
 export const USER_RADIUS = 10;
