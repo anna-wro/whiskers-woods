cb008a79803d35941d074391e7cd551740a989d3
Anna Wróbel <wro.online@gmail.com>
2023-07-18 15:48:58 +0200

    🤗 Messages to the user

--- src/components/copy.ts
+++
@@ -1,3 +0,0 @@
-export const copy = {
-    title: 'Lost Kitty',
-}
---
+++ src/consts/copy.ts
@@ -0,0 +1,5 @@
+export const copy = {
+    title: 'Whiskers in the woods',
+    tip: `Someone's a little bit shy, try pressing the arrows!`,
+    afterKeypress: `Awww, you found me! Wanna walk?`
+}
