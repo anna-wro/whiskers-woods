f9388ed67a7168b3b429f054775a45a3cada5636
anna-wro <wro.online@gmail.com>
2023-08-10 14:36:27 +0200

    fix styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Map2D from './components/Map2D';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './index.css';
 
 const container = document.getElementById('app-root');
 const root = createRoot(container);
