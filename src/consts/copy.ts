a0dea51e174ce42ba9b4ce5fa8fd8827794aa0ce
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-08-10 14:39:59 +0200

    🏗️ Refactor Map2D, adjust config (#2)

--- src/consts/copy.ts
+++ src/consts/copy.ts
@@ -1,5 +1,5 @@
 export const copy = {
-  title: "Whiskers in the woods",
+  title: 'Whiskers in the woods',
   tip: `I'm a little bit shy, try pressing the arrows!`,
   afterKeypress: `Awww, you found me! Wanna walk?`,
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Map2D from "./components/Map2D";
-import styles from "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Map2D from './components/Map2D';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './index.css';
 
-const container = document.getElementById("app-root");
+const container = document.getElementById('app-root');
 const root = createRoot(container);
 root.render(<Map2D />);
